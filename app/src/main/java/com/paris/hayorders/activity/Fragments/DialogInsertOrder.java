f6bd361277d2ad3af33d1bb04cf3f4e355b09ce3
renanparis <paris.oliveira@gmail.com>
2019-07-10 14:36:09 -0300

    Refatoração do InsertDialogFragment.

--- app/src/main/java/com/paris/hayorders/activity/Fragments/DialogInsertOrder.java
+++ app/src/main/java/com/paris/hayorders/activity/Fragments/DialogInsertOrder.java
@@ -1,71 +1,60 @@
 package com.paris.hayorders.activity.Fragments;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-import com.google.android.material.textfield.TextInputLayout;
 import com.paris.hayorders.R;
 
 public class DialogInsertOrder extends DialogFragment{
 
-    private TextView title;
     private EditText fieldOrder;
-    private TextView cancel;
-    private TextView confirm;
     private InputOrderListener listener;
 
     public interface InputOrderListener{
         void sendInputOrder(String input);
     }
 
-
-    @Nullable
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.dialog_fragment_insert_order, container, false);
-        title = view.findViewById(R.id.dialog_fragment_title);
-        TextInputLayout inputLayout = view.findViewById(R.id.dialog_fragment_field_order);
-        fieldOrder = inputLayout.getEditText();
-        cancel = view.findViewById(R.id.dialog_fragment_cancel);
-        confirm = view.findViewById(R.id.dialog_fragment_confirm);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        LayoutInflater inflater = getActivity().getLayoutInflater();
 
+        View view = inflater.inflate(R.layout.dialog_fragment_insert_order, null);
 
-        return view;
-    }
+        fieldOrder = view.findViewById(R.id.dialog_fragment_field_order);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        cancel.setOnClickListener(new View.OnClickListener() {
+        builder.setView(view)
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getDialog().dismiss();
+                    }
+                }).setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                getDialog().dismiss();
-            }
-        });
-
-        confirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            public void onClick(DialogInterface dialog, int which) {
 
                 String input = fieldOrder.getText().toString();
+
                 listener.sendInputOrder(input);
-                getDialog().dismiss();
             }
         });
 
+
+        return builder.create();
     }
 
     @Override
